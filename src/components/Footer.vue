180028f0dc1fc83e9fd95522c45178140e57ebfe
gdlyeabkov <gdlyeabkov@gmail.com>
2021-12-02 11:37:41 +0300

    addheaderandfooter

---
+++ src/components/Footer.vue
@@ -0,0 +1,219 @@
+<template>
+    <div class="footer">
+        &nbsp;
+        <div class="footerItem footerTop">
+            <img class="footerTopLogo" width="150px" src="https://static1.smartbear.co/swagger/media/images/logos/sw_full-wt.svg" alt="" />
+            <div class="footerTopColumns">
+                <div class="footerTopColumn">
+                    <span class="footerTopColumnItem footerTopColumnHeader">
+                        Swagger Open Source
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Open Source License
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger Forum
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger IRC
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger Community
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger Projects
+                    </span>
+                </div>
+                <div class="footerTopColumn">
+                    <span class="footerTopColumnItem footerTopColumnHeader">
+                        Swagger
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        About Swagger                        
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Blog
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Support
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        News
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Contact Us
+                    </span>
+                </div>
+                <div class="footerTopColumn">
+                    <span class="footerTopColumnItem footerTopColumnHeader">
+                        Pro Tools
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        SwaggerHub
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger Inspector
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        SwaggerHub Enterprise
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        SwaggerHub vs OSS
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        SwaggerHub Integrations
+                    </span>
+                </div>
+                <div class="footerTopColumn">
+                    <span class="footerTopColumnItem footerTopColumnHeader">
+                        Resources
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        OpenAPI Specification
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Resources
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Open Source Docs
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        Swagger Inspector Docs
+                    </span>
+                    <span class="footerTopColumnItem footerTopColumn">
+                        
+                        SwaggerHub Docs
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="footerItem footerMiddle">
+            <div>
+                <span class="footerMiddleHeaderLabel footerMiddleHeaderLabelFirst">
+                    Explore
+                </span>
+                <span class="footerMiddleHeaderLabel">
+                    SmartBear
+                </span>
+                <span class="footerMiddleHeaderLabel">
+                    Tools
+                </span>
+            </div>
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <div>
+                    <span>
+                        Contact Us | 
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        +1 617-684-2600 USA|
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        +353 91 398300 EUR|
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        +61 391929960 AUS
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="footerItem footerBottom">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    
+    .footer {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        height: 1000px;
+    }
+
+    .footerItem {
+        
+    }
+
+    .footerTop {
+        box-sizing: border-box;
+        padding: 75px;
+        display: flex;
+        flex-direction: column;
+        height: 450px;
+        background-size: cover;
+        background-position: 0% 50%;
+        background-image: url('https://static1.smartbear.co/swagger/media/images/backgrounds/photos/sw-ws-bgphoto5.png');    
+    }
+
+    .footerMiddle {
+        background-color: rgb(0, 50, 100);
+        height: 500px;
+        box-sizing: border-box;
+        padding: 35px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footerBottom {
+        background-color: rgb(50, 50, 50);
+        height: 165px;
+    }
+
+    .footerMiddleHeaderLabel {
+        color: rgb(255, 255, 255);
+        font-size: 22px;
+    }
+
+    .footerMiddleHeaderLabelFirst {
+        text-decoration: underline;
+        text-underline-offset: 25px;
+        text-decoration-color: rgb(200, 150, 0);
+    }
+
+    .footerTopLogo {
+        /* margin-top: 75px; */
+    }
+
+    .footerTopColumn {
+        display: flex;
+        flex-direction: column;
+        margin: 0px 35px;
+    }
+
+    .footerTopColumnHeader {
+        color: rgb(0, 150, 0) !important;
+        font-size: 24px !important;
+        margin: 5px !important;
+    }
+
+    .footerTopColumnItem {
+        color: rgb(255, 255, 255);
+        font-size: 18px;
+        margin: 3px;
+    }
+
+    .footerTopColumns {
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="header">
+        <div class="headerItem headerItemLeft">
+            <img class="headerItemElement" src="https://static1.smartbear.co/swagger/media/assets/images/swagger_logo.svg" alt="" width="165px" />
+            <div class="headerItemElement">
+                <span class="headerItemElementItem headerItemElementItemLabel">
+                    Why Appier
+                </span>
+                <span class="material-icons headerItemElementItem headerItemElementItemIcon">
+                    expand_more
+                </span>
+            </div>
+            <div class="headerItemElement">
+                <span class="headerItemElementItem headerItemElementItemLabel">
+                    Tools
+                </span>
+                <span class="material-icons headerItemElementItem headerItemElementItemIcon">
+                    expand_more
+                </span>
+            </div>
+            <div class="headerItemElement">
+                <span class="headerItemElementItem headerItemElementItemLabel">
+                    Resources
+                </span>
+                <span class="material-icons headerItemElementItem headerItemElementItemIcon">
+                    expand_more
+                </span>
+            </div>
+        </div>
+        <div class="headerItem headerItemRight">
+            <div class="headerItemElement">
+                <span class="material-icons">
+                    search
+                </span>
+            </div>
+            <div class="headerItemElement">
+                <button class="btn btn-light signInBtn">
+                    Sign In
+                </button>
+            </div>
+            <div class="headerItemElement">
+                <button class="btn btn-success">
+                    Try Free
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    
+    .header {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 65px;
+        background-color: rgb(255, 255, 255);
+        box-shadow: 0px 0px 5px rgb(200, 200, 200);
+        display: flex;
+        justify-content: space-between;
+        box-sizing: border-box;
+        padding: 0px 85px;
+    }
+
+    .headerItem {
+        display: flex;
+        align-items: center;
+    }
+
+    .headerItemElement {
+        display: flex;
+        align-items: center;
+        margin: 10px;
+    }
+
+    .headerItemElementItem {
+        margin: 0px 5px;
+    }
+
+    .headerItemElementItemLabel {
+        font-size: 18px;
+    }
+
+    .headerItemElementItemIcon {
+        font-size: 28px;
+        font-weight: normal;
+    }
+    
+    .headerItemLeft {
+        width: 65%;
+        align-items: center;
+    }
+
+    .signInBtn {
+        border: 1px solid rgb(0, 150, 0);
+        background-color: rgb(255, 255, 255);
+    }
+
+</style>
